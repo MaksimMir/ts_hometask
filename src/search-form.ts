499a1900bf5df704a5446941f1b062152b2cb0c3
MaxM <mak-miroshnichen@yandex.ru>
2021-12-25 13:13:42 +0300

    task1

---
+++ src/search-form.ts
@@ -0,0 +1,53 @@
+import { renderBlock } from './lib.js';
+
+const setDate = (day: number): string => {
+  const newDay = new Date().setDate(new Date().getDate() + day);
+  const date =  new Date(newDay).toISOString().split('T');
+  return date[0];
+}
+
+const ENTRY_DATE = setDate(1);
+const DEPARTTURE_DATE = setDate(3);
+const MIN_DATE = new Date();
+const MAX_DATE = setDate(60);
+
+export function renderSearchFormBlock (entryDate: string = ENTRY_DATE, departureDate: string = DEPARTTURE_DATE): void {
+
+  renderBlock(
+    'search-form-block',
+    `
+    <form>
+      <fieldset class="search-filedset">
+        <div class="row">
+          <div>
+            <label for="city">Город</label>
+            <input id="city" type="text" disabled value="Санкт-Петербург" />
+            <input type="hidden" disabled value="59.9386,30.3141" />
+          </div>
+          <!--<div class="providers">
+            <label><input type="checkbox" name="provider" value="homy" checked /> Homy</label>
+            <label><input type="checkbox" name="provider" value="flat-rent" checked /> FlatRent</label>
+          </div>--!>
+        </div>
+        <div class="row">
+          <div>
+            <label for="check-in-date">Дата заезда</label>
+            <input id="check-in-date" type="date" value="${entryDate}" min="${MIN_DATE}" max="${MAX_DATE}" name="checkin" />
+          </div>
+          <div>
+            <label for="check-out-date">Дата выезда</label>
+            <input id="check-out-date" type="date" value="${departureDate}" min="${MIN_DATE}" max="${MAX_DATE}" name="checkout" />
+          </div>
+          <div>
+            <label for="max-price">Макс. цена суток</label>
+            <input id="max-price" type="text" value="" name="price" class="max-price" />
+          </div>
+          <div>
+            <div><button>Найти</button></div>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+    `
+  )
+}
